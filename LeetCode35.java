2bbec35990ccea5cb64759709452501f4c3ed31a
narendramongam <mongamnarendra@gmail.com>
2024-07-20 20:45:46 +0530

    leetcode35

--- LeetCode35.java
+++ LeetCode35.java
@@ -1,7 +1,21 @@
 package Leetcode;
 
+import java.util.Arrays;
+
 public class LeetCode35 {
     public static void main(String[] args) {
-
+        int[] nums = {3,5,7,9,10};
+        int target = 7;
+        System.out.println(searchInsert(nums,target));
     }
+    public static int searchInsert(int[] nums, int target) {
+        int index = Arrays.binarySearch(nums, target);
+
+        if (index < 0) {
+            index = -index - 1;
+        }
+        return index;
+    }
+
+
 }

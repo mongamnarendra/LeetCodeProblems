f0ce0ef5783e6c2466105860d2fd183761531238
narendramongam <mongamnarendra@gmail.com>
2024-07-24 07:32:09 +0530

    Remove Element

---
+++ LeetCode27.java
@@ -0,0 +1,30 @@
+package Leetcode;
+
+public class LeetCode27 {
+    public static void main(String[] args) {
+        int[] nums={0,1,2,2,3,0,4,2};
+        int val = 2;
+        System.out.println(removeElement(nums,val));
+    }
+    public static int removeElement(int[] nums, int val) {
+
+        int i =0;
+        int j = nums.length-1;
+        int count =0;
+        while(i<=j)
+        {
+            if(nums[i]==val)
+            {
+                int temp =nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j--;
+                count++;
+            }
+            else {
+                i++;
+            }
+        }
+        return nums.length-count;
+    }
+}

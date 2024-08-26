c43288e5ee708b1f31c7357c46fd86b25bb6e1d1
narendramongam <mongamnarendra@gmail.com>
2024-08-26 11:39:46 +0530

    Search Insert

--- LeetCode35.java
+++ LeetCode35.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class LeetCode35 {
     public static void main(String[] args) {
         int[] nums = {3,5,7,9,10};
-        int target = 7;
+        int target = 3;
         System.out.println(searchInsert(nums,target));
     }
     public static int searchInsert(int[] nums, int target) {

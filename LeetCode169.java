e41958d2e5fe15cce63cb714aa8b0e38a3b05325
narendramongam <mongamnarendra@gmail.com>
2024-08-27 20:48:24 +0530

    Majority Element

---
+++ LeetCode169.java
@@ -0,0 +1,27 @@
+package LeetCodeProblems;
+import java.util.HashMap;
+public class LeetCode169 {
+    public static void main(String[] args) {
+        System.out.println(majorityElement(new int[] {2,2,1,1,1,2,2}));
+    }
+    public static int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> hm  =new HashMap<>();
+        for(int i:nums)
+        {
+            hm.put(i,hm.getOrDefault(i,0)+1);
+        }
+        int res =0;
+        int major=0;
+        for(int i:nums)
+        {
+            int n = hm.get(i);
+            if(n>res)
+            {
+                major=i;
+                res=hm.get(i);
+            }
+        }
+
+        return major;
+    }
+}

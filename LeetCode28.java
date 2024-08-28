bffde7075f3744ded457296605a44ba48d89b069
narendramongam <mongamnarendra@gmail.com>
2024-08-28 19:43:14 +0530

    Find the index of the first occurence in string

--- LeetCode28.java
+++ LeetCode28.java
@@ -10,4 +10,22 @@ public class LeetCode28 {
         return haystack.indexOf(needle);
     }
 
+//    public static int strStr(String haystack, String needle) {
+//        if(needle.isEmpty())
+//        {
+//            return 0;
+//        }
+//        if(needle.length()>haystack.length())
+//        {
+//            return -1;
+//        }
+//        for(int i=0;i<=haystack.length()-needle.length();i++)
+//        {
+//            if(haystack.substring(i,i+needle.length()).equals(needle))
+//            {
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
 }

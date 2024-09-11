a8d023bca47ace1d47c1399b0bcfe862054096c4
narendramongam <mongamnarendra@gmail.com>
2024-09-11 12:28:32 +0530

    Sqrt(x)

---
+++ LeeetCode69.java
@@ -0,0 +1,28 @@
+package LeetCodeProblems;
+
+public class LeeetCode69 {
+    public static void main(String[] args) {
+        System.out.println(mySqrt(4));
+    }
+    public static int mySqrt(int x) {
+        if (x == 0 || x == 1)
+            return x;
+        int start = 1;
+        int end = x;
+        while(start <= end)
+        {
+            int mid=start+(end - start)/2;
+            if(mid*mid == x)
+            {
+                return mid;
+            }
+            else if((long)mid*mid > (long)x){
+                end =  mid-1;
+            }
+            else {
+                start = mid+1;
+            }
+        }
+        return Math.round(end);
+    }
+}
